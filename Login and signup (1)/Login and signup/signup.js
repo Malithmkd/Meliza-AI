f0b327384c139aeef9dddd0649b175ed8b4a6764
Malithmkd <malith.kavidu.34@gmail.com>
2024-12-06 22:26:44 +0530

    updates

--- Login and signup (1)/Login and signup/signup.js
+++
@@ -1,5 +0,0 @@
-function togglePassword(id) {
-  const passwordField = document.getElementById(id);
-  const type = passwordField.type === 'password' ? 'text' : 'password';
-  passwordField.type = type;
-}
--- Pixel Hunters/Pixel Hunters/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('button:not(.current)');
-    
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            alert(`You selected the ${button.innerText} plan!`);
-        });
-    });
-});
